0d3229e514f122df0c945771cd6e8ae51c497b99
Zohan Momin <zohanaiderak@gmail.com>
2020-06-22 08:43:28 -0400

    deploy run build

---
+++ client/build/precache-manifest.59ad88b24b1534f8e70eaf15f262b3d5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f8eff90c2844ce545784335eb95e8ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b39fc5755f54215f0dfc",
+    "url": "/static/css/main.9c71fd7d.chunk.css"
+  },
+  {
+    "revision": "95c3028934b7cf45b760",
+    "url": "/static/js/2.c6754866.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c6754866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b39fc5755f54215f0dfc",
+    "url": "/static/js/main.80fca916.chunk.js"
+  },
+  {
+    "revision": "ec9ede1065cc12a71d4c",
+    "url": "/static/js/runtime-main.d0eb0f73.js"
+  },
+  {
+    "revision": "6ec340c03aae5891264203ae324d1f87",
+    "url": "/static/media/Icon-instagram.6ec340c0.svg"
+  },
+  {
+    "revision": "5205f0bd4594a93c1911acf5be2f6d0e",
+    "url": "/static/media/Linotype - Avenir Next LT Pro Bold.5205f0bd.ttf"
+  },
+  {
+    "revision": "c6f15a45c14921dc49cc3fdcf7dd15af",
+    "url": "/static/media/Linotype - Avenir Next LT Pro Demi.c6f15a45.ttf"
+  },
+  {
+    "revision": "3092616e8ff42b73a73ba548fd30044c",
+    "url": "/static/media/Linotype - Avenir Next LT Pro Light.3092616e.ttf"
+  },
+  {
+    "revision": "551b138c48d98e4a931edae361999b33",
+    "url": "/static/media/Linotype - Avenir Next LT Pro Medium.551b138c.ttf"
+  },
+  {
+    "revision": "f3c6a359493c478244e4d90048b81b3f",
+    "url": "/static/media/Linotype - Avenir Next LT Pro.f3c6a359.ttf"
+  },
+  {
+    "revision": "222586f42fa280e476462914cdf70dec",
+    "url": "/static/media/Linotype - AvenirNextLTPro-Bold.222586f4.otf"
+  },
+  {
+    "revision": "1a1c2c769bb1a94529f507e96d6985ac",
+    "url": "/static/media/Linotype - AvenirNextLTPro-Demi.1a1c2c76.otf"
+  },
+  {
+    "revision": "808733d84b493990f27275dc0204ee71",
+    "url": "/static/media/Linotype - AvenirNextLTPro-Light.808733d8.otf"
+  },
+  {
+    "revision": "f6560f5e526c7a2211b1972527028532",
+    "url": "/static/media/Linotype - AvenirNextLTPro-Medium.f6560f5e.otf"
+  },
+  {
+    "revision": "67016125d8abf229098a0195c7f9ed64",
+    "url": "/static/media/Linotype - AvenirNextLTPro-Regular.67016125.otf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d5e155eea5cf036dc5c6639a6bf94e62",
+    "url": "/static/media/msnap-logo.d5e155ee.png"
+  },
+  {
+    "revision": "f5f56ebce826aedaf255d34e54a053ea",
+    "url": "/static/media/msnap-logo.f5f56ebc.png"
+  }
+]);
